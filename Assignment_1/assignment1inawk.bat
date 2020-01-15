e5ce4375d9b2776a6559f3c5893a5a722fe2c3f5
Syed Shah MD. Adib <48805976+4D1B@users.noreply.github.com>
2020-01-15 16:54:08 +0600

    Updated Assignment-1_in_AWK

--- Assignment_1/assignment1inawk.bat
+++ Assignment_1/assignment1inawk.bat
@@ -1,6 +1,6 @@
 echo on
  
- awk -F , "NR>1 {a[$1\" \"$7]++} END{for(b in a){arr[i+1]=b; i++} count=i;maleCount=0;femaleCount=0; for(j=0;j<count;j++){if(index(arr[j],\"MALE\")){maleCount++; } if(index(arr[j],\"FEMALE\")){femaleCount++; }}print femaleCount \" is the total female count and total male count is \" maleCount-femaleCount;}" h:/ad.csv
+ awk -F "\"*,\"*" "NR>1 {a[$1\" \"$7]++} END{for(b in a){arr[i+1]=b; i++} count=i;maleCount=0;femaleCount=0; for(j=0;j<count;j++){if(index(arr[j],\"MALE\")){maleCount++; } if(index(arr[j],\"FEMALE\")){femaleCount++; }}print femaleCount \" is the total female count and total male count is \" maleCount-femaleCount;}" h:/ad.csv
  
  cmd /k
  
